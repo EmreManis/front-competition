98ec4ec71450ed7784a6833bade767517d14b4f2
Emre <emremn07@gmail.com>
2021-12-18 15:39:28 +0300

    carousel finished

--- components/tourSection/Carousel.js
+++ components/tourSection/Carousel.js
@@ -10,10 +10,23 @@ import "swiper/css/navigation";
 import classes from "./carousel.module.css";
 
 import tatilBudur from "../../public/tour/tour1.png";
+import forwardArrow from "../../public/carousel/forwardArrow.png";
+import backArrow from "../../public/carousel/backArrow.png";
 
 const Slide = () => {
+  const [swiper, setSwiper] = useState();
+  const prevRef = useRef();
+  const nextRef = useRef();
 
-  
+  useEffect(() => {
+    if (swiper) {
+      console.log("Swiper instance:", prevRef.current);
+      swiper.params.navigation.prevEl = prevRef.current;
+      swiper.params.navigation.nextEl = nextRef.current;
+      swiper.navigation.init();
+      swiper.navigation.update();
+    }
+  }, [swiper]);
 
   let slide = [];
   for (let i = 0; i <= 8; i++) {
@@ -29,18 +42,25 @@ const Slide = () => {
 
   return (
     <div className="flex mx-10">
-    
+      <div className="swiper-button flex justify-center items-center" ref={prevRef}>
+      <Image src={backArrow} width={18} height={18} alt="tatil-budur" />
+      </div>
       <Swiper
         modules={[Navigation]}
         slidesPerView={4}
         // navigation={true}
         className={classes.swiper}
-        navigation={true}
- 
+        navigation={{
+          prevEl: prevRef?.current,
+          nextEl: nextRef?.current,
+        }}
+        onSwiper={setSwiper}
       >
         {slide}
       </Swiper>
-     
+      <div className="swiper-button flex justify-center items-center" ref={nextRef}>
+        <Image src={forwardArrow} width={18} height={18} alt="tatil-budur" />
+      </div>
     </div>
   );
 };
